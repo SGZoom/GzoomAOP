9b5073394f0fab8eb68f998b2fc946e4a1b87642
liamjyxu <liamjyxu@tencent.com>
2020-06-15 22:45:24 +0800

    框架搭建提交

---
+++ GZoomAopPlugin/src/main/java/com/gzoom/aopplugin/GZoomAopPlugin.java
@@ -0,0 +1,11 @@
+package com.gzoom.aopplugin;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class GZoomAopPlugin implements Plugin<Project> {
+    @Override
+    public void apply(Project project) {
+
+    }
+}
